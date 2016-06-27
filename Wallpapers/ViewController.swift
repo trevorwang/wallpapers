c52525817d1645df34d637af3d4b3ff9038da38e
Ming Wang <wangming@dajiazhongyi.com>
2016-06-27 18:05:20 +0800

    Initial Commit

---
+++ Wallpapers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Wallpapers
+//
+//  Created by Ming Wang on 6/27/16.
+//  Copyright © 2016 Mingsin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
